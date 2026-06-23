746d9760ef518c6f5957b973e34fa52d363bd240
n20186005-hash <n20186005@gmail.com>
2026-06-23 10:52:30 +0800

    更新

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 export default function robots(): MetadataRoute.Robots {
   const domain = process.env.CURRENT_SITE_DOMAIN || 'www.plazadefranciapanama.com';
   const baseUrl = `https://${domain}`;
